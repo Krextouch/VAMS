925fe38bf76de04fa3764935df337c6dec20fb9c
Krextouch <kevbob@live.de>
2022-03-16 19:04:21 +0100

    init project

---
+++ backend/src/main/java/edu/dhbw/stuttgart/tinf20b/vamsBE/VamsBeApplication.java
@@ -0,0 +1,13 @@
+package edu.dhbw.stuttgart.tinf20b.vamsBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VamsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VamsBeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/dhbw/stuttgart/tinf20b/vamsBE/VamsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dhbw.stuttgart.tinf20b.vamsBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VamsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
